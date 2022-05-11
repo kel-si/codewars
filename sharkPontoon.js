46e2ca8e23447d15c0d1dc691cbb364f5283d55c
Kelsi Olstad <kelsiolstad@gmail.com>
2022-05-10 20:19:24 -0600

    complete solution

---
+++ sharkPontoon.js
@@ -0,0 +1,40 @@
+// Instructions:
+// Your friend invites you out to a cool floating pontoon around 1km off the beach. Among other things, the pontoon has a huge slide that drops you out right into the ocean, a small way from a set of stairs used to climb out.
+
+// As you plunge out of the slide into the water, you see a shark hovering in the darkness under the pontoon... Crap!
+
+// You need to work out if the shark will get to you before you can get to the pontoon. To make it easier... as you do the mental calculations in the water you either freeze when you realise you are dead, or swim when you realise you can make it!
+
+// You are given 5 variables;
+
+// sharkDistance = distance from the shark to the pontoon. The shark will eat you if it reaches you before you escape to the pontoon.
+
+// sharkSpeed = how fast it can move in metres/second.
+
+// pontoonDistance = how far you need to swim to safety in metres.
+
+// youSpeed = how fast you can swim in metres/second.
+
+// dolphin = a boolean, if true, you can half the swimming speed of the shark as the dolphin will attack it.
+
+// The pontoon, you, and the shark are all aligned in one dimension.
+
+// If you make it, return "Alive!", if not, return "Shark Bait!".
+
+// Solution:
+
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) {
+  let calcSharkSpeed = sharkSpeed;
+
+  if (dolphin) {
+    calcSharkSpeed = calcSharkSpeed / 2;
+  }
+
+  if (pontoonDistance / youSpeed < sharkDistance / calcSharkSpeed) {
+    return "Alive!";
+  } else {
+    return "Shark Bait!";
+  }
+}
+
+/* Note: I made an assumption that the shark is directly behind the swimmer, not on the opposite side of the boat for example */
