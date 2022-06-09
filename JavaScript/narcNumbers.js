370466e8050ee50959e4c0b2e912821ff3d11285
Kelsi Olstad <kelsiolstad@gmail.com>
2022-06-08 21:56:55 -0600

    correct instructions

--- JavaScript/narcNumbers.js
+++ JavaScript/narcNumbers.js
@@ -1,7 +1,7 @@
 //Instructions: A Narcissistic Number is a number of length l in which the sum of its digits to the power of l is equal to the original number. If this seems confusing, refer to the example below.
 
 // Ex: 153, where l = 3 ( the number of digits in 153 )
-// 13 + 53 + 33 = 153
+// 1^3 + 5^3 + 3^3 = 153
 
 // Write a function that, given n, returns whether or not n is a Narcissistic Number.
 
