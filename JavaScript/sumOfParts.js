7815af43668e54ec743fca9a63c800e175c32978
Kelsi Olstad <kelsiolstad@gmail.com>
2022-06-30 15:12:59 -0600

    refactor to try to make code more efficient - works on small cases but still times out on very large inputs

--- JavaScript/sumOfParts.js
+++ JavaScript/sumOfParts.js
@@ -1,5 +1,9 @@
 function partsSums(ls) {
-  let sums = [];
+  if (ls.length === 0) {
+    return [0];
+  }
+
+  let sums = [0];
 
   let initialSum = 0;
   for (let i = 0; i < ls.length; i++) {
@@ -7,18 +11,25 @@ function partsSums(ls) {
   }
   sums.push(initialSum);
 
-  function arraySum(arr) {
-    if (arr.length > 0) {
-      let sum = sums[sums.length - 1] - arr[0];
-      sums.push(sum);
-      arr.shift();
-      return arraySum(arr);
-    } else {
-      sums.push(0);
-      return sums;
-    }
+  for (let i = ls.length - 1; i > 0; i--) {
+    initialSum -= ls[0];
+    sums.push(initialSum);
+    ls.shift();
   }
-  return arraySum(ls);
+  return sums.sort((a, b) => b - a);
+
+  // function arraySum(arr) {
+  //   if (arr.length > 0) {
+  //     let sum = sums[sums.length - 1] - arr[0];
+  //     sums.push(sum);
+  //     arr.shift();
+  //     return arraySum(arr);
+  //   } else {
+  //     sums.push(0);
+  //     return sums;
+  //   }
+  // }
+  // return arraySum(ls);
 }
 
 console.log(partsSums([0, 1, 3, 6, 10])); // [20, 20, 19, 16, 10, 0]
