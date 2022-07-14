a6b905c15c8eda15430e7f947d41a49041093efc
Kelsi Olstad <kelsiolstad@gmail.com>
2022-07-13 21:57:19 -0600

    complete -- find difference in ASCII values when replacing 7s with 1s.

--- JavaScript/charCodeCalculation.js
+++ JavaScript/charCodeCalculation.js
@@ -1,28 +1,22 @@
 function calc(x) {
-  // not finished!!
-
-  let arr = [];
+  let str = "";
   for (const char of x) {
-    arr.push(char.charCodeAt().toString());
+    str += char.charCodeAt().toString();
   }
 
-  // array with numbers as strings, 7 replaced with 1
-  const replaceSeven = arr.map((x) => x.replace(/7/g, "1"));
-  console.log("replace7", replaceSeven);
+  const onesStr = str.replace(/7/g, "1");
 
-  const joinReplaceStr = replaceSeven.join("");
-  console.log("join", typeof joinReplaceStr);
+  let num = 0;
+  for (const char of str) {
+    num += parseInt(char);
+  }
 
-  const allReplaceDigits = Array.from(joinReplaceStr);
-  console.log(allReplaceDigits);
+  let onesNum = 0;
+  for (const char of onesStr) {
+    onesNum += parseInt(char);
+  }
 
-  // add all digits in both
-
-  // const replacedSeven = parseInt(arr.join("").replace(/7/g, "1"));
-  // const initial = parseInt(arr.join(""));
-
-  // console.log("initial------", initial);
-  // console.log("replacedSeven", replacedSeven);
+  return num - onesNum;
 }
 
-console.log(calc("Aabcdef"));
+console.log(calc("ABC"));
