a6635f3e503491c4a75a001cb8532d459c6a80b1
Kelsi Olstad <kelsiolstad@gmail.com>
2022-09-19 14:43:35 -0600

    refactor

--- JavaScript/level7/sumOfAllArguments.js
+++ JavaScript/level7/sumOfAllArguments.js
@@ -1,9 +1,15 @@
+// Write a function that finds the sum of all its arguments.
+
 function sum(...args) {
-  let argSum = 0;
-  for (let i = 0; i < args.length; i++) {
-    argSum += args[i];
-  }
-  return argSum;
+  // let argSum = 0;
+  // for (let i = 0; i < args.length; i++) {
+  //   argSum += args[i];
+  // }
+  // return argSum;
+
+  //Refactor:
+  // args is an array:
+  return args.reduce((a, b) => a + b, 0);
 }
 
 console.log(sum(1, 2, 3)); // 5
